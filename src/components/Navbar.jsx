1dc341f8fa69dc955cb78dd62f3d029d5e4097c1
Sudip Raut <sudipraut444@gmail.com>
2024-12-19 14:06:30 +0545

    Completed searchcomponent and search functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,15 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { assets } from "../assets/assets";
 import { Link, Links, NavLink, useNavigate } from "react-router-dom";
+import { ShopContext } from "../context/ShopContext";
 const Navbar = () => {
   const [visible, setVisible] = useState(false);
-
+  
+  const{setShowSearch}=useContext(ShopContext)
   return (
     <div className="flex items-center justify-between py-5 font-medium">
       <Link to="/">
-        <img src={assets.logo} alt="LogoImage" className="w-40" />
+        <img  src={assets.logo} alt="LogoImage" className="w-40" />
       </Link>
       <ul className="hidden sm:flex gap-5  text-sm text-gray-600">
         <NavLink to="/" className="flex flex-col items-center gap-1">
@@ -29,6 +31,7 @@ const Navbar = () => {
       </ul>
       <div className="flex items-center gap-6">
         <img
+        onClick={()=>setShowSearch(true)}
           src={assets.search_icon}
           alt="searchicone"
           className="w-5 cursor-pointer"
